3ae80ada53b89bcc93a20b4e36d64c8108d4ad61
ngoxuantu <ngoxuantu9812@gmail.com>
2019-01-25 15:39:12 +0700

    first commit

---
+++ database/migrations/2016_06_09_021546_create_news_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateNewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('TinTuc', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('author')->default('Ngô Xuân Tú');
+            $table->text('summary');
+            $table->longText('content');
+            $table->string('image');
+            $table->integer('highlight')->default(1);
+            $table->integer('view')->default(0);
+            $table->integer('category_id');
+            $table->integer('status')->default(1);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('TinTuc');
+    }
+}
